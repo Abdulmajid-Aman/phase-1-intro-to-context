840a7ab7fc5b093b36c43cbef4f07c0b34bc7fda
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-07-11 19:00:05 +0300

    context lab

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    return employee
+}
+
+function createEmployeeRecords(arrays) {
+   return arrays.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+  
+    return employeeRecord;
+  }
+
+
+//   function createTimeOutEvent(employeeRecord, dateStamp) {
+//     let [date, hour] = dateStamp.split(' ');
+  
+//     employeeRecord.timeOutEvents.push({
+//       type: "TimeIn",
+//       hour: parseInt(hour, 10),
+//       date: date
+//     });
+  
+//     return employeeRecord;
+//   }
+function createTimeOutEvent(employeeRecord, dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+  
+    return employeeRecord;
+  }
+  
+
+
+  function hoursWorkedOnDate(employeeRecord, date) {
+  let timeIn = employeeRecord.timeInEvents.find(event => event.date === date);
+  let timeOut = employeeRecord.timeOutEvents.find(event => event.date === date);
+
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    let hours = hoursWorkedOnDate(employeeRecord, date);
+    return hours * employeeRecord.payPerHour;
+  }
+  
+  
+
+  function allWagesFor(employeeRecord) {
+    let dates = employeeRecord.timeInEvents.map(event => event.date);
+  
+    return dates.reduce((total, date) => {
+      return total + wagesEarnedOnDate(employeeRecord, date);
+    }, 0);
+  }
+
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, record) => {
+      return total + allWagesFor(record);
+    }, 0);
+  }
+  
+  
